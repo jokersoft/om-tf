9becf6ee9c7d4813bfbae4b1647d4fa8c42296c8
Yaroslav Sklabinkyi <yaroslav.sklabinskyi@auto1.com>
2019-11-01 13:35:03 +0100

    tf apply works

---
+++ terraform/alb.tf
@@ -0,0 +1,51 @@
+resource "aws_lb" "lb" {
+  name               = "test-lb"
+  load_balancer_type = "application"
+  internal           = true
+
+  security_groups = ["sg-b60e82dd"]
+  subnets         = ["subnet-09644a43", "subnet-838dd3eb", "subnet-f4d1b48e"]
+
+  tags {
+    environment = "default"
+    service     = "test-service"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.tg.arn}"
+  }
+
+  load_balancer_arn = "${aws_lb.lb.arn}"
+  port              = 80
+  protocol          = "HTTP"
+}
+
+//resource "aws_lb_listener" "https" {
+//  "default_action" {
+//    type = "forward"
+//    target_group_arn = "${aws_lb_target_group.tg.arn}"
+//  }
+//
+//  load_balancer_arn = "${aws_lb.lb.arn}"
+//  port = 443
+//  protocol = "HTTPS"
+//}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "test-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-a171ecc9"
+
+  health_check {
+    interval            = 15
+    timeout             = 10
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = 200
+    path                = "/management/health"
+  }
+}
---
+++ terraform/ecs.tf
@@ -0,0 +1,26 @@
+data "aws_ecs_cluster" "cluster" {
+  cluster_name = "eu-central-1-main"
+}
+
+resource "aws_ecs_task_definition" "service" {
+  container_definitions = "${file("./service.json")}"
+  family                = "test-family"
+}
+
+resource "aws_ecs_service" "service" {
+  cluster         = "${data.aws_ecs_cluster.cluster.arn}"
+  name            = "test"
+  desired_count   = 1
+  task_definition = "${aws_ecs_task_definition.service.arn}"
+
+  load_balancer {
+    container_name   = "testserver"
+    container_port   = 8080
+    target_group_arn = "${aws_lb_target_group.tg.arn}"
+  }
+
+//  tags {
+//    environment = "default"
+//    service     = "test-service"
+//  }
+}
